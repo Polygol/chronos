b6a87df8cab67a3fd7154bc319a1fc2754c36d2d
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 10:18:16 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,113 @@
+const LANG_EN_CHRONOS = {
+    ALARM: "Alarm",
+    TIMER: "Timer",
+    STOPWATCH: "Stopwatch",
+    WORLDS: "Worlds",
+    ADD_ALARM: "Add Alarm",
+    START: "Start",
+    STOP: "Stop",
+    LAP: "Lap",
+    RESET: "Reset",
+    ADD_CLOCK: "Add Clock",
+    DISMISS_TIMER: "Dismiss",
+    DISMISS_ALARM: "Dismiss",
+    TIMER_FINISHED_TITLE: "Timer",
+    TIMER_FINISHED_BODY: "Your timer has finished!",
+    ALARM_TITLE: "Alarm",
+    ALARM_NOTIFICATION_TITLE: "Alarm • {time}"
+};
+
+const LANG_JP_CHRONOS = {
+    ALARM: "アラーム",
+    TIMER: "タイマー",
+    STOPWATCH: "ストップウォッチ",
+    WORLDS: "世界時計",
+    ADD_ALARM: "アラームを追加",
+    START: "開始",
+    STOP: "停止",
+    LAP: "ラップ",
+    RESET: "リセット",
+    ADD_CLOCK: "時計を追加",
+    DISMISS_TIMER: "閉じる",
+    DISMISS_ALARM: "閉じる",
+    TIMER_FINISHED_TITLE: "タイマー",
+    TIMER_FINISHED_BODY: "タイマーが終了しました！",
+    ALARM_TITLE: "アラーム",
+    ALARM_NOTIFICATION_TITLE: "アラーム • {time}"
+};
+
+const LANG_DE_CHRONOS = {
+    ALARM: "Alarm",
+    TIMER: "Timer",
+    STOPWATCH: "Stoppuhr",
+    WORLDS: "Weltzeituhr",
+    ADD_ALARM: "Alarm hinzufügen",
+    START: "Start",
+    STOP: "Stopp",
+    LAP: "Runde",
+    RESET: "Zurücksetzen",
+    ADD_CLOCK: "Uhr hinzufügen",
+    DISMISS_TIMER: "Schließen",
+    DISMISS_ALARM: "Schließen",
+    TIMER_FINISHED_TITLE: "Timer",
+    TIMER_FINISHED_BODY: "Ihr Timer ist abgelaufen!",
+    ALARM_TITLE: "Alarm",
+    ALARM_NOTIFICATION_TITLE: "Alarm • {time}"
+};
+
+const LANG_ES_CHRONOS = {
+    ALARM: "Alarma",
+    TIMER: "Temporizador",
+    STOPWATCH: "Cronómetro",
+    WORLDS: "Reloj Mundial",
+    ADD_ALARM: "Añadir Alarma",
+    START: "Iniciar",
+    STOP: "Detener",
+    LAP: "Vuelta",
+    RESET: "Reiniciar",
+    ADD_CLOCK: "Añadir Reloj",
+    DISMISS_TIMER: "Descartar",
+    DISMISS_ALARM: "Descartar",
+    TIMER_FINISHED_TITLE: "Temporizador",
+    TIMER_FINISHED_BODY: "¡Tu temporizador ha terminado!",
+    ALARM_TITLE: "Alarma",
+    ALARM_NOTIFICATION_TITLE: "Alarma • {time}"
+};
+
+const LANG_KO_CHRONOS = {
+    ALARM: "알람",
+    TIMER: "타이머",
+    STOPWATCH: "스톱워치",
+    WORLDS: "세계 시계",
+    ADD_ALARM: "알람 추가",
+    START: "시작",
+    STOP: "정지",
+    LAP: "랩",
+    RESET: "초기화",
+    ADD_CLOCK: "시계 추가",
+    DISMISS_TIMER: "닫기",
+    DISMISS_ALARM: "닫기",
+    TIMER_FINISHED_TITLE: "타이머",
+    TIMER_FINISHED_BODY: "타이머가 종료되었습니다!",
+    ALARM_TITLE: "알람",
+    ALARM_NOTIFICATION_TITLE: "알람 • {time}"
+};
+
+const LANG_ZH_CHRONOS = {
+    ALARM: "闹钟",
+    TIMER: "计时器",
+    STOPWATCH: "秒表",
+    WORLDS: "世界时钟",
+    ADD_ALARM: "添加闹钟",
+    START: "开始",
+    STOP: "停止",
+    LAP: "计次",
+    RESET: "重置",
+    ADD_CLOCK: "添加时钟",
+    DISMISS_TIMER: "关闭",
+    DISMISS_ALARM: "关闭",
+    TIMER_FINISHED_TITLE: "计时器",
+    TIMER_FINISHED_BODY: "您的计时器已结束！",
+    ALARM_TITLE: "闹钟",
+    ALARM_NOTIFICATION_TITLE: "闹钟 • {time}"
+};
